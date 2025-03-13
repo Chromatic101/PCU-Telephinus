2fff8d9f5c91f3b0c06ccfb136e31635fc35f0fd
Chromatic101 <twomroblox@gmail.com>
2025-03-13 13:45:17 +0800

    Updated TPS.js

--- TPS.js
+++ TPS.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const suddenAnnouncementButton = document.getElementById('suddenAnnouncementButton');
     const logoutButton = document.getElementById('logoutButton');
 
-    // Redirect to Sudden Announcement page (Calendar)
+    
     suddenAnnouncementButton.addEventListener('click', function () {
-        window.location.href = 'TPAnnouncement.html';  // Redirect to Announcements page
+        window.location.href = 'TPAnnouncement.html'; 
     });
 
-    // Log out functionality
+   
     logoutButton.addEventListener('click', function () {
         alert('Logged out successfully!');
-        window.location.href = 'index.html'; // Redirect to login page
+        window.location.href = 'index.html';
     });
 });
